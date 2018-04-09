f3f183b6965f66229927a99f31be74d071a6228c
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-04-09 20:52:55 +0300

    Add files via upload

---
+++ RastgeleUygulama1/RastgeleUygulama1/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SayiTextBox = New System.Windows.Forms.TextBox()
+        Me.Yontem1Button = New System.Windows.Forms.Button()
+        Me.Yontem2Button = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SayiTextBox
+        '
+        Me.SayiTextBox.Location = New System.Drawing.Point(12, 21)
+        Me.SayiTextBox.Name = "SayiTextBox"
+        Me.SayiTextBox.Size = New System.Drawing.Size(162, 20)
+        Me.SayiTextBox.TabIndex = 0
+        '
+        'Yontem1Button
+        '
+        Me.Yontem1Button.Location = New System.Drawing.Point(12, 57)
+        Me.Yontem1Button.Name = "Yontem1Button"
+        Me.Yontem1Button.Size = New System.Drawing.Size(162, 47)
+        Me.Yontem1Button.TabIndex = 1
+        Me.Yontem1Button.Text = "Rastgele Sayı Üret Yöntem 1"
+        Me.Yontem1Button.UseVisualStyleBackColor = True
+        '
+        'Yontem2Button
+        '
+        Me.Yontem2Button.Location = New System.Drawing.Point(14, 107)
+        Me.Yontem2Button.Name = "Yontem2Button"
+        Me.Yontem2Button.Size = New System.Drawing.Size(162, 47)
+        Me.Yontem2Button.TabIndex = 2
+        Me.Yontem2Button.Text = "Rastgele Sayı Üret Yöntem 2"
+        Me.Yontem2Button.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(190, 261)
+        Me.Controls.Add(Me.Yontem2Button)
+        Me.Controls.Add(Me.Yontem1Button)
+        Me.Controls.Add(Me.SayiTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Rastgele"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SayiTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Yontem1Button As System.Windows.Forms.Button
+    Friend WithEvents Yontem2Button As System.Windows.Forms.Button
+
+End Class
---
+++ RastgeleUygulama1/RastgeleUygulama1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Yontem1Button_Click(sender As Object, e As EventArgs) Handles Yontem1Button.Click
+        Dim Sayi As Decimal
+        'Sayi = Rnd()
+        'Ürettilen 0 ile 1 arasındaki ondalık sayıyı daha büyük ve tam sayıya dönüştürme
+        'Sayi = CInt(Rnd() * 100) '0 ile 100 arasında sayılar üretir
+        'Sayi = CInt(Rnd() * 5) + 5 ' 5 ile 10 arasında sayı üretir
+        Sayi = CInt(Rnd() * 1000) Mod 6 + 5 ' 5 ile 10 arasında sayı üretir
+        'Büyük sayıdan küçük sayıyı çıkart +1 ekle Mod'un önüne yaz + dan sonra küçük sayıyı yaz
+        SayiTextBox.Text = Sayi
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Randomize()
+    End Sub
+
+    Private Sub Yontem2Button_Click(sender As Object, e As EventArgs) Handles Yontem2Button.Click
+        Dim Sayi As Decimal
+        Dim Rastgele As New Random
+        'Sayi = Rastgele.Next ' negatif olmayan integer sayılar üretir
+        'Sayi = Rastgele.Next(10) '0 ile 10 arasında saylar üretir
+        Sayi = Rastgele.Next(10, 20) '10 ile 20 arasında saylar üretir
+        SayiTextBox.Text = Sayi
+    End Sub
+End Class
